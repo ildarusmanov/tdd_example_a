b8baccdb3a0b94d0b45a5182a770659b05a9fa49
Ildar Usmanov <wajox@mail.ru>
2018-07-28 00:37:48 +0300

    create red test .Sum()

---
+++ calculator_test.go
@@ -0,0 +1,29 @@
+package main_test
+
+import (
+	. "github.com/ildarusmanov/tdd_example_a"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Calculator", func() {
+	var (
+		a          int
+		b          int
+		calculator *Calculator
+	)
+
+	BeforeEach(func() {
+		calculator = CreateNewCalculator()
+		a = 5
+		b = 10
+	})
+
+	Describe("Sum()", func() {
+		Context("With a and b", func() {
+			It("should be equal to expected sum", func() {
+				Expect(calculator.Sum(a, b)).To(Equal(a + b))
+			})
+		})
+	})
+})
