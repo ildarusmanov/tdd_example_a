32b58da2e0e24cdc0bb429913a2f47642d1f9e1d
Ildar Usmanov <wajox@mail.ru>
2018-07-28 00:41:10 +0300

    add new red tests for .Sum()

--- calculator_test.go
+++ calculator_test.go
@@ -23,6 +23,8 @@ var _ = Describe("Calculator", func() {
 		Context("With a and b", func() {
 			It("should be equal to expected sum", func() {
 				Expect(calculator.Sum(a, b)).To(Equal(a + b))
+				Expect(calculator.Sum(a, 1)).To(Equal(a + 1))
+				Expect(calculator.Sum(b, 1)).To(Equal(b + 1))
 			})
 		})
 	})
